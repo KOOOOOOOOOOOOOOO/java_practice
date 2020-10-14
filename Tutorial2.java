a11e032c847aa21f757963fd4202d5dc41877798
구승희 <pdw05460@gmail.com>
2020-10-14 14:19:53 +0900

    Java DataType

---
+++ Tutorial2.java
@@ -0,0 +1,17 @@
+public class Tutorial2 {
+    // int,double,String 등 자료형은 나타낼 수 있는 한계가 있다..
+    // int형이 가질 수 있는 최대 값은 약 21억, 최솟값도 -21억정도 
+    // final static double PI = 3.141592;
+    // final은 절대 변하지 않는 상수.. main 함수보다 먼저 실행
+    public static void main(String[] args) {
+        
+        int a =1;
+        int b =2;
+
+        System.out.println("a+b = "+(a+b));
+        System.out.println("a-b = "+(a-b));
+        System.out.println("a*b = "+(a*b));
+        System.out.println("a/b = "+(a/b));
+    }
+    
+}
