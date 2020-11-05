f9d76c91cec063647ccacd329744616f59dadc1e
구승희 <pdw05460@gmail.com>
2020-11-05 14:30:42 +0900

    Java if&else and loof

---
+++ Tutorial5.java
@@ -0,0 +1,17 @@
+public class Tutorial5 {
+    public static void main(String[] args) {
+
+        String a = "I Love you";
+        if (a.contains("Love"))
+        {
+            //포함하는 경우 실행되는 부분
+            System.out.println("Me too");
+        } 
+        else
+        {
+            //포함하지 않는 경우 실행되는 경우
+            System.out.println("I hate you");
+        }
+        
+    }
+}
